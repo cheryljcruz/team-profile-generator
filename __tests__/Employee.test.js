2767ba97525d5e44187b39faa5b04cba89bb9624
koiclone <cheryljcruz1@gmail.com>
2022-04-12 18:55:26 -0500

    Added first employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,10 @@
 const Employee = require('../lib/Employee.js');
+
+// creates employee object
+test('creates an employee object', () => {
+    const employee = new Employee('Bob', 7080997, 'bobthebuilder@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
