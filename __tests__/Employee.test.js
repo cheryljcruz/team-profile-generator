7507fa25d47f287db77e1bdebb28d3266aa33a28
koiclone <cheryljcruz1@gmail.com>
2022-04-12 19:47:25 -0500

    added the rest of Employee test: name, id, email, employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,3 @@
-const { test } = require('@jest/globals');
 const Employee = require('../lib/Employee.js');
 
 // creates employee object
@@ -10,3 +9,30 @@ test('creates an employee object', () => {
     expect(employee.email).toEqual(expect.any(String));
 });
 
+// employee name
+test('gets employee name', () => {
+    const employee = new Employee('Bob', 7080997, 'bobthebuilder@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+// employee ID
+test('gets employee ID', () => {
+    const employee = new Employee('Bob', 7080997, 'bobthebuilder@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+// employee email 
+test('gets employee email', () => {
+    const employee = new Employee('Bob', 7080997, 'bobthebuilder@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+// employee role
+test('gets role of employee', () => {
+    const employee = new Employee('Bob', 7080997, 'bobthebuilder@gmail.com');
+
+    expect(employee.getRole()).toEqual('Employee');
+});
