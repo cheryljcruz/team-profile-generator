003193a550d499aa1bc8c963ab42d1443fa3cdad
koiclone <cheryljcruz1@gmail.com>
2022-04-13 20:38:12 -0500

    complete manager prompt questions

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ const Employee = require('./lib/Employee.js');
 // array to store objects
 // const teamArr = [];
 
-// prompt manager, engineer, intern
-
+// prompt manager
 const promptManager = () => {
     inquirer.prompt([
         {
@@ -28,8 +27,47 @@ const promptManager = () => {
                     return false;
                 }
             }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your employee ID? (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an employee ID!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the Manager's email address? (Required)",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an email address!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'Enter your office number: ',
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an office number!');
+                    return false;
+                }
+            }
         }
     ])
 };
 
-promptManager();
+console.log(promptManager());
